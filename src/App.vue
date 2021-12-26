fbe01e6696b486ab5f94c95cab65a161aa756510
Dibimo <diogobissolimoreno@gmail.com>
2021-12-26 10:50:14 -0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ nomeApp }}</h1>
+    <img
+      :src="foto.url"
+      :alt="foto.titulo"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      nomeApp: "Alurapic",
+      foto:{
+        url: "https://www.petlove.com.br/dicas/wp-content/uploads/2021/08/Gato-filhote-7.jpg",
+        titulo:"gatinho fofo"
+      }
+    }
+  }
+};
+</script>
+
+<style></style>
