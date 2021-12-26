7b9ab65a0e0208414b6772832f0ef36e150be1c6
Dibimo <diogobissolimoreno@gmail.com>
2021-12-26 11:09:22 -0300

    v-for implementado para lista de imgs

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <h1>{{ nomeApp }}</h1>
-    <img
-      :src="foto.url"
-      :alt="foto.titulo"
-    />
+    <ul>
+      <li v-for="foto in fotos" :key="foto.titulo">
+        <img :src="foto.url" :alt="foto.titulo" :key="foto.titulo">
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -13,10 +14,16 @@ export default {
   data(){
     return{
       nomeApp: "Alurapic",
-      foto:{
-        url: "https://www.petlove.com.br/dicas/wp-content/uploads/2021/08/Gato-filhote-7.jpg",
-        titulo:"gatinho fofo"
-      }
+      fotos:[
+        {
+          url: "https://www.petlove.com.br/dicas/wp-content/uploads/2021/08/Gato-filhote-7.jpg",
+          titulo:"gatinho fofo"
+        },
+        {
+          url: "https://i.pinimg.com/236x/45/24/de/4524de2715890053f73b997f80688118.jpg",
+          titulo:"outro gatinho fofo"
+        }
+      ]
     }
   }
 };
